52af70f897b451b32216e436dfd838d63efc9c2b
Leon Georgi <leon@ifsr.de>
2020-01-31 10:32:23 +0100

    Initial Commit

---
+++ Mensa/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Mensa
+//
+//  Created by Leon on 31.01.20.
+//  Copyright © 2020 Leon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
